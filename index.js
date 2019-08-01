4a4d3c98f04f08a6f12f9ff66447da7ca34bb749
Lohrem <30306978+Lohrem@users.noreply.github.com>
2019-08-01 02:41:31 -0400

    test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server')
 require('dotenv').config()
-const port = process.env.PORT ? process.env.PORT : 4000
+const port = process.env.PORT ? process.env.PORT : 4000;
 
 server.listen(post, () => {
   console.log(`running on port ${port}`);
